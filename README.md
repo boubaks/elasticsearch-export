5a52856ea514200d947fc34dadbb824d477516eb
boubaks <soufiane.boubakour@epitech.eu>
2016-11-16 00:50:13 +0100

    adding elasticsearch-query & elasticsearch-client on export tool & clean for npm

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-elasticsearch-export
-====================
+# elasticsearch-export
+elasticsearch-export exports elasticsearch datas into JSON files
+
+## Installation
+
+$> npm install -g elasticsearch-export
+
+$> elasticsearch-export
+
+## Launch elasticsearch-export
+
+	$> elasticsearch-export --help
+	Usage: node elasticsearch-export.js
+
+	  -i, --index=ARG     index to export (default to all)
+	  -t, --type=ARG      type to export
+	  -q, --query=ARG     export by a query
+	  -e, --elsQuery=ARG  export by a elasticsearch query
+	  -o, --output=ARG    name of file where the JSON will be extract
+	  -s, --size=ARG      get this number of entities
+	  -f, --from=ARG      get entities from this number
+	      --jsonELS       get the json with the _source
+	  -P, --port=ARG      port to connect to
+	  -H, --host=ARG      server to connect to
+	  -h, --help          display this help
+	  -v, --version       show version
+
+
+
+## How to use the elasticsearch-export
+    
+    $> elasticsearch-export --output export.json --index test --type test
+
+    You can use the --jsonELS option if you want to get only the _source on every
+    document on your elasticsearch
+  
+## Notes
+
+For import data to elasticsearch you can use the elasticsearch-import tool.
+
+More information on : https://github.com/boubaks/elasticsearch-import
--- src/elasticsearch-export.js
+++ src/elasticsearch-export.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
 var fs = require('fs');
 var getopt = require('node-getopt');
 var logger = require(__dirname + '/../lib/logger');
-var ELSCLIENT = require(__dirname + '/../lib/ElsClient').ElsClient;
-var ELSQUERY = require(__dirname + '/../lib/ElsQuery').ElsQuery;
+var elsClient = require('elasticsearch-client');
+var elsQuery = require('elasticsearch-query');
 
 var opt = getopt.create([
     ['i', 'index=ARG', 'index to export (default to all)'],
@@ -13,8 +14,8 @@ var opt = getopt.create([
     ['s', 'size=ARG', 'get this number of entities'],
     ['f', 'from=ARG', 'get entities from this number'],
     ['', 'jsonELS', 'get the json with the _source'],
-    ['P', 'port=ARG', 'port to connect to'],
-    ['H', 'host=ARG', 'server to connect to'],
+    ['P', 'port=ARG', 'port to connect to (default to 9200)'],
+    ['H', 'host=ARG', 'server to connect to (default to localhost)'],
     ['h', 'help', 'display this help'],
     ['v', 'version', 'show version']
 ])
@@ -29,87 +30,94 @@ var host = opt.options.host ? opt.options.host : 'localhost';
 var query = opt.options.query ? opt.options.query : {};
 var index = opt.options.index ? opt.options.index : '_all';
 var type = opt.options.type ? opt.options.type : index;
-var output = opt.options.output ? opt.options.output : 'output';
-var size = opt.options.size ? opt.options.size : 10;
+var output = opt.options.output ? opt.options.output : 'output.json';
+var size = opt.options.size ? opt.options.size : 'all';
 var from = opt.options.from ? opt.options.from : 0;
 var jsonELS = opt.options.jsonELS ? opt.options.jsonELS : null;
-var elsQuery = opt.options.elsQuery ? opt.options.elsQuery : null;
+var manualQueryELS = opt.options.elsQuery ? opt.options.elsQuery : null;
 
 /*
 ** Initialization elasticsearch client & query
 */
 function writeData(res, callback) {
-    if (!jsonELS) {
-	fs.writeFileSync(output, JSON.stringify(res.hits.hits));
-	callback(null);
-    } else {
-	var datas = [];
-	for (iterator in res.hits.hits) {
-	    res.hits.hits[iterator]._source._id = res.hits.hits[iterator]._id;
-	    datas.push(res.hits.hits[iterator]._source);
+	console.log('res', res);
+	if (res && res.hits && res.hits.total > 0) {
+	    if (!jsonELS) {
+			fs.writeFileSync(output, JSON.stringify(res.hits.hits));
+			callback(null);
+	    } else {
+			var datas = [];
+			for (iterator in res.hits.hits) {
+			    res.hits.hits[iterator]._source._id = res.hits.hits[iterator]._id;
+			    datas.push(res.hits.hits[iterator]._source);
+			}
+			fs.writeFileSync(output, JSON.stringify(datas));
+			callback(null);
+	    }
+	} else {
+		callback(null);
 	}
-	fs.writeFileSync(output, JSON.stringify(datas));
-	callback(null);
-    }
 }
 
-new ELSCLIENT(host, port, function(elsClient, msg) {
-    if (!elsClient)
-	throw('Couldn\'t connect to ELS');
+new elsClient(host, port, function(client, msg) {
+    if (!client)
+		throw ('Couldn\'t connect to ELS');
     var scope = this;
-    var elsQueryM = undefined;
-    new ELSQUERY(function(tmpQuery) {
-		elsQueryM = tmpQuery;
+    var elsQueryGenerator = undefined;
+    new elsQuery(function(tmpQuery) {
+		elsQueryGenerator = tmpQuery;
     });
 
     var options = {
-	size: size,
-	from: from
+		size: size,
+		from: from
     };
 
     if (!opt.options.size) {
-	elsQueryM.generate(type, query, null, {term: true}, function(err, queryELS) {
-	    if (err) {
-		console.log(err);
-	    } else {
-	    if (elsQuery) {
-	    	try {
-	    		console.log('elsQuery', elsQuery);
-				query = JSON.parse(elsQuery);
-			} catch (e) {
-				console.log(e);
-				query = {};
-			}
-		} else {
-			query = queryELS;
-		}
-		console.log('query', JSON.stringify(query));
-		elsClient.count(index, query, function(err, result) {
-			console.log('count', result);
-		    size = result.count;
-		    options.size = size;
-		    if (size > 0) {
-			elsClient.search(index, query, options, function(err, res) {
-			    logger.info('elasticsearch-exported=> '+ size + ' files exported on file ' + output);
-			    writeData(res, function(err) {
-				if (err)
-				    console.log(err);
-				process.kill();				
-			    });
-
-			});
+		elsQueryGenerator.generate(type, query, null, {term: true}, function(err, queryELS) {
+		    if (err) {
+				console.log(err);
 		    } else {
-			logger.info('elasticsearch-exported=> no file exported');
-			process.kill();
+			    if (manualQueryELS) {
+			    	try {
+			    		console.log('manualQueryELS', manualQueryELS);
+						query = JSON.parse(manualQueryELS);
+					} catch (e) {
+						console.log(e);
+						query = {};
+					}
+				} else {
+					query = queryELS;
+				}
+				console.log('query', JSON.stringify(query));
+				client.count(index, query, options, function(err, result) {
+					console.log('count', result);
+					if (size === 'all') {
+					    size = result.count;
+					    options.size = size;
+					}
+				    if (size > 0 && result.count > 0) {
+	    			    logger.info('elasticsearch-exported => ' + size + ' files exported on file ' + output);
+						client.search(index, query, options, function(err, res) {
+						    writeData(res, function(err) {
+							if (err)
+							    console.log(err);
+								process.kill(process.pid);				
+						    });
+
+						});
+				    } else {
+						logger.info('elasticsearch-exported => no file exported');
+						process.kill(process.pid);
+				    }
+				});
 		    }
 		});
-	    }
-	});
     } else {
-	elsClient.search(index, query, options, function(err, res) {
-	    logger.info('elasticsearch-exported=> '+ size + ' files exported on file ' + output);
-	    fs.writeFileSync(output, JSON.stringify(res.hits.hits));
-	    process.kill();
-	});
+		client.search(index, query, options, function(err, res) {
+		    logger.info('elasticsearch-exported => '+ size + ' files exported on file ' + output);
+		    fs.writeFileSync(output, JSON.stringify(res.hits.hits));
+		    process.kill(process.pid);
+		});
     }
 });
